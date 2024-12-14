a69690dd06247f6250d2972a984e3a87d458f1dc
Gábor Librecz <kuglee@gmail.com>
2024-12-14 10:31:25 +0100

    Initial Commit

---
+++ SwiftUIHeroTransitionExperiment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIHeroTransitionExperiment
+//
+//  Created by Librecz Gábor on 2024. 12. 14..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIHeroTransitionExperiment/SwiftUIHeroTransitionExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHeroTransitionExperimentApp.swift
+//  SwiftUIHeroTransitionExperiment
+//
+//  Created by Librecz Gábor on 2024. 12. 14..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHeroTransitionExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
